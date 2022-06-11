d6dada81799ee0a32c2af79ab7de9fad21f06a3d
Ivan Gadžić <igadzic@foi.hr>
2022-06-11 15:22:55 +0200

    no message

---
+++ Software/E-videoteka/Autentifikator.cs
@@ -0,0 +1,78 @@
+﻿namespace E_videoteka
+{
+    public class Autentifikator
+    {
+        public RepozitorijKorisnika repozitorij = new RepozitorijKorisnika();
+        public void Registracija(string ime, string prezime, string email, string username, string password, string passwordrep)
+        {
+            ProvjeriLozinku(password, passwordrep);
+            VerificirajRegistraciju(ime, prezime, email, username, password, passwordrep);
+            repozitorij.DodajKorisnika(ime, prezime, email, username, password);
+        }
+        private void ProvjeriLozinku(string password, string passwordrep)
+        {
+            if (password != passwordrep)
+            {
+                throw new PasswordMissMatchException("Lozinke nisu iste");
+            }
+        }
+
+        public void VerificirajRegistraciju(string ime, string prezime, string email, string username, string password, string passwordrep)
+        {
+            if (ime == "")
+            {
+                throw new InvalidInputException("Polje Ime ne može biti prazno");
+            }
+            if (prezime == "")
+            {
+                throw new InvalidInputException("Polje Prezime ne može biti prazno");
+            }
+            if (email == "")
+            {
+                throw new InvalidInputException("Polje E-mail ne može biti prazno");
+            }
+            if (username == "")
+            {
+                throw new InvalidInputException("Polje Username ne može biti prazno");
+            }
+            if (password == "")
+            {
+                throw new InvalidInputException("Polje Lozinka ne može biti prazno");
+            }
+            if (passwordrep == "")
+            {
+                throw new InvalidInputException("Polje Ponovljena lozinka ne može biti prazno");
+            }
+        }
+        public bool ProvjeriPrijavu(string username, string password)
+        {
+            using (var context = new PI2247_DBEntities2())
+            {
+                var query = from p in context.Korisniks
+                            where p.Username == username && p.Password == password
+                            select p;
+
+                if (query.Any())
+                {
+                    System.Windows.Forms.MessageBox.Show("Uspjesšno ste ulogirani.");
+                    return true;
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Krivo korisnicko ime ili lozinka.");
+                    return false;
+                }
+            }
+        }
+
+        public void ProvjeraUloge()
+        {
+            using (var context = new PI2247_DBEntities2())
+            {
+                var query = from p in context.Korisnik
+                            where p.Uloga == "Korisnik"
+                            select p;
+            }
+        }
+    }
+}
---
+++ Software/E-videoteka/RepozitorijKorisnika.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace E_videoteka
+{
+    public class RepozitorijKorisnika
+    {
+        public void DodajKorisnika(string ime, string prezime, string email, string username, string password)
+        {
+            using (var context = new PI2247_DBEntities2())
+            {
+                Korisnik korisnik = new Korisnik
+                {
+                    ID_Korisnik = 10,
+                    Ime = ime,
+                    Prezime = prezime,
+                    Email = email,
+                    Username = username,
+                    Password = password,
+                    Uloga = "Korisnik",
+                };
+                context.Korisniks.Add(korisnik);
+                context.SaveChanges();
+            }
+        }
+    }
+}
