c254b6f0315017759137f49b1e9a159ec96d67e5
DominikB0 <96126900+DominikB0@users.noreply.github.com>
2022-06-26 16:23:52 +0200

    Update frmPrijava.Designer.cs

--- Software/E-videoteka/frmPrijava.Designer.cs
+++ Software/E-videoteka/frmPrijava.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbUsername = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtbUsername = new System.Windows.Forms.TextBox();
             this.txtbPassword = new System.Windows.Forms.TextBox();
@@ -37,46 +37,51 @@
             this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label1
+            // lbUsername
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(19, 43);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Korisničko ime:";
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Location = new System.Drawing.Point(25, 53);
+            this.lbUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(145, 25);
+            this.lbUsername.TabIndex = 0;
+            this.lbUsername.Text = "Korisničko ime:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(65, 87);
+            this.label2.Location = new System.Drawing.Point(87, 107);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.Size = new System.Drawing.Size(86, 25);
             this.label2.TabIndex = 1;
             this.label2.Text = "Lozinka:";
             // 
             // txtbUsername
             // 
-            this.txtbUsername.Location = new System.Drawing.Point(144, 45);
+            this.txtbUsername.Location = new System.Drawing.Point(192, 55);
+            this.txtbUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtbUsername.Name = "txtbUsername";
-            this.txtbUsername.Size = new System.Drawing.Size(179, 20);
+            this.txtbUsername.Size = new System.Drawing.Size(237, 22);
             this.txtbUsername.TabIndex = 2;
             // 
             // txtbPassword
             // 
-            this.txtbPassword.Location = new System.Drawing.Point(144, 89);
+            this.txtbPassword.Location = new System.Drawing.Point(192, 110);
+            this.txtbPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtbPassword.Name = "txtbPassword";
             this.txtbPassword.PasswordChar = '*';
-            this.txtbPassword.Size = new System.Drawing.Size(179, 20);
+            this.txtbPassword.Size = new System.Drawing.Size(237, 22);
             this.txtbPassword.TabIndex = 3;
             // 
             // btnPrijava
             // 
-            this.btnPrijava.Location = new System.Drawing.Point(99, 192);
+            this.btnPrijava.Location = new System.Drawing.Point(132, 236);
+            this.btnPrijava.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrijava.Name = "btnPrijava";
-            this.btnPrijava.Size = new System.Drawing.Size(106, 52);
+            this.btnPrijava.Size = new System.Drawing.Size(141, 64);
             this.btnPrijava.TabIndex = 4;
             this.btnPrijava.Text = "Prijavi se";
             this.btnPrijava.UseVisualStyleBackColor = true;
@@ -84,9 +89,10 @@
             // 
             // btnOdustani
             // 
-            this.btnOdustani.Location = new System.Drawing.Point(281, 192);
+            this.btnOdustani.Location = new System.Drawing.Point(375, 236);
+            this.btnOdustani.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnOdustani.Name = "btnOdustani";
-            this.btnOdustani.Size = new System.Drawing.Size(107, 52);
+            this.btnOdustani.Size = new System.Drawing.Size(143, 64);
             this.btnOdustani.TabIndex = 5;
             this.btnOdustani.Text = "Odustani";
             this.btnOdustani.UseVisualStyleBackColor = true;
@@ -97,25 +103,27 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label3.Location = new System.Drawing.Point(52, 128);
+            this.label3.Location = new System.Drawing.Point(69, 158);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(243, 16);
+            this.label3.Size = new System.Drawing.Size(308, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "Nemate korisnički račun? Registriraj se!";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // frmPrijava
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(412, 256);
+            this.ClientSize = new System.Drawing.Size(549, 315);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnOdustani);
             this.Controls.Add(this.btnPrijava);
             this.Controls.Add(this.txtbPassword);
             this.Controls.Add(this.txtbUsername);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbUsername);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmPrijava";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Prijava";
@@ -128,7 +136,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbUsername;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtbUsername;
         private System.Windows.Forms.TextBox txtbPassword;
